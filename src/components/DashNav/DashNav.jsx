8e05657ec300403b34df2dfc27162dc3561f46fc
Prince <princepr356@gmail.com>
2025-10-30 14:58:30 -0400

    made some changes in dashboard

--- src/components/DashNav/DashNav.jsx
+++ src/components/DashNav/DashNav.jsx
@@ -4,22 +4,20 @@ import { NavLink } from "react-router-dom";
 const DashNav = () => {
   return (
     <div className="flex flex-col w-80 bg-slate-50 p-4 min-h-screen">
-      {/* User Info */}
       <div className="flex gap-3 mb-6">
         <div
           className="bg-center bg-no-repeat aspect-square bg-cover rounded-full w-10 h-10"
           style={{
             backgroundImage:
-              'url("https://lh3.googleusercontent.com/aida-public/AB6AXuCzlOfJCRVMC2J5ZsNahN-raxXGcC4BhsP8WkjzS-xz_uwkZfaW8oz6Tv0GGMXRXm3C-IapTNePusjzYcvOMVXKrEPzFBuUokSa3ADO7xQla6RS8TAPlIaprglZdw8hDNQ6mu9A3pxWOMz-EogUUSJzKKU7AHt75GGakdbU1OajueOkFvtgR1Fys2oFFX1J186rKbAYFAdlM6dbKtwIXgerkoteEeXPqIpUn-rUhYWvyQrDy9g-YvVEiyjsq-KqJgGGWGds4l9e4vA")',
+              'url("https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png")',
           }}
         ></div>
         <div className="flex flex-col justify-center">
-          <h1 className="text-[#0d171b] text-base font-medium">Olivia Harper</h1>
-          <p className="text-[#4c809a] text-sm font-normal">Female</p>
+          <h1 className="text-[#0d171b] text-base font-medium">ANK Zaman</h1>
+          <p className="text-[#4c809a] text-sm font-normal">Male</p>
         </div>
       </div>
 
-      {/* Main Menu */}
       <div className="flex flex-col gap-2">
         <NavItem label="Overview" iconClass="fas fa-home" to="/dashboard" end />
         <NavItem label="Medication" iconClass="fas fa-pills" to="/dashboard/medication" />
@@ -28,7 +26,6 @@ const DashNav = () => {
         <NavItem label="Settings" iconClass="fas fa-cog" to="/dashboard/settings" />
       </div>
 
-      {/* Bottom Menu */}
       <div className="mt-auto flex flex-col gap-2">
         <NavItem label="Help & FAQ" iconClass="fas fa-question-circle" to="/dashboard/help" />
         <NavItem label="Feedback" iconClass="fas fa-comment-dots" to="/dashboard/feedback" />
