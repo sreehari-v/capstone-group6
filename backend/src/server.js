c5527f3a88f7f379ec233a0bf53f11b69ff36a08
Prince <princepr356@gmail.com>
2025-11-02 15:40:04 -0500

    Add backend scaffold with Express server

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,11 @@ const express = require('express');
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("<h1>Server running</h1>");
+});
 
 app.listen(5000, (req, res) => {
-    res.send("Running");
+    console.log("🚀 Server is up and running... \n✅ http://localhost:5000\n");
+    //✅   http://localhost:4000/graphql\n\n
 });
