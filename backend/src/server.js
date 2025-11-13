aa662bf8608feb63302f66c61df6ad4577e37a00
Viswanathan Lakshmanan <vishwanathanlaksh@gmail.com>
2025-11-12 20:50:43 -0500

    Merged Successfully

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // -----------------------------
-// 🔌 Connect to MongoDB
+// Connect to MongoDB
 // -----------------------------
 connectDB();
 
